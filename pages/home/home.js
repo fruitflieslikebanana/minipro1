3d9ce21b8694c668d5cb964a3f1edd07b122a1a4
fruitflieslikebanana <846277849@qq.com>
2020-01-31 16:15:47 +0800

    项目*3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,24 @@
 // pages/home/home.js
 Page({
-
+  data:{
+    name:'Teletubbies',
+    age: 18,
+    student:[
+      {id: 1, name:'dingding',color:'purple'},
+      {id: 2, name:'dixi',color:'green'},
+      {id: 3, name:'lala',color:'yellow'},
+      {id: 4, name:'paul',color:'red'}
+    ],
+    counter:0
+  },
+  handleBtnClick(){
+    this.setData({
+      counter:this.data.counter +1
+    })
+  },
+  handleSubstraction(){
+    this.setData({
+      counter: this.data.counter -1
+    })
+  }
 })
